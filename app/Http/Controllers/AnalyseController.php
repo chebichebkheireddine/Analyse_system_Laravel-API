ccc780e0bd07ef7730947429d325f688d3e0d6a4
kheireddine.chebicheb <kheireddine.chebicheb@univ-tiaret.dz>
2024-01-20 08:11:54 +0100

    Auth system V2

---
+++ app/Http/Controllers/AnalyseController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Analyse;
+use App\Http\Requests\StoreAnalyseRequest;
+use App\Http\Requests\UpdateAnalyseRequest;
+
+class AnalyseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAnalyseRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Analyse $analyse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Analyse $analyse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAnalyseRequest $request, Analyse $analyse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Analyse $analyse)
+    {
+        //
+    }
+}
